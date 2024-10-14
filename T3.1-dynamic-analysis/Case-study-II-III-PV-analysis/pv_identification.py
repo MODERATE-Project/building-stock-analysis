0751f5ca360b8ae74ebfc428e912cd1b75969976
PhilippMaschi <phil.masch@hotmail.com>
2024-10-14 20:59:31 +0200

    cropping all images to rooftop size because otherwise many buildings in one image

--- T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/pv_identification.py
+++ T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/pv_identification.py
@@ -57,42 +57,41 @@ def cut_tif_into_building_photos(buildings, src, imsize: int):
             bounds = building['geometry'].bounds
             min_px, min_py = src.index(bounds[0], bounds[1])  # minx, miny
             max_px, max_py = src.index(bounds[2], bounds[3]) 
-            building_width_px = max_px - min_px
-            building_height_px = max_py - min_py
+            if min_px > max_px:
+                min_px, max_px = max_px, min_px
 
-            if building_width_px <= imsize and building_height_px <= imsize:
-                center_px, center_py = src.index(centroid.x, centroid.y)
+            if min_py > max_py:
+                min_py, max_py = max_py, min_py
+            # if building_width_px <= imsize and building_height_px <= imsize:
+            #     center_px, center_py = src.index(centroid.x, centroid.y)
 
-                px_min = center_px - imsize / 2
-                px_max = center_px + imsize / 2
-                py_min = center_py - imsize / 2
-                py_max = center_py + imsize / 2
+            #     px_min = center_px - imsize / 2
+            #     px_max = center_px + imsize / 2
+            #     py_min = center_py - imsize / 2
+            #     py_max = center_py + imsize / 2
 
-            else:
-                px_min = min_px
-                px_max = max_px
-                py_min = min_py
-                py_max = max_py
+            # else:
+            #     px_min = min_px
+            #     px_max = max_px
+            #     py_min = min_py
+            #     py_max = max_py
 
 
             # Ensure the pixel coordinates are within image bounds
-            px_min = max(0, px_min)
-            px_max = min(orig_file.shape[1], px_max)
-            py_min = max(0, py_min)
-            py_max = min(orig_file.shape[2], py_max)
+            px_min = max(0, min_px)
+            px_max = min(orig_file.shape[1], max_px)
+            py_min = max(0, min_py)
+            py_max = min(orig_file.shape[2], max_py)
             
             clipped_orgfile = orig_file[:3, int(px_min):int(px_max), int(py_min):int(py_max)]  # CAREFUL, this is writte for RGBI images, cutting the infrared part
 
-            # If the building was too large, resize it back to 224x224
-            if clipped_orgfile.shape[1] != imsize or clipped_orgfile.shape[2] != imsize:
-                # Convert to PIL Image, resize, then convert back to NumPy array
-                img = np.moveaxis(clipped_orgfile, 0, -1)  # Rearrange (bands, x, y) to (x, y, bands)
-                img = Image.fromarray(img.astype('uint8'))  # Convert to unsigned 8-bit integer format
-                img_resized = img.resize((imsize, imsize), Image.LANCZOS)
-                clipped_orgfile = np.moveaxis(np.array(img_resized), -1, 0) 
-                print(f"{building.osmid} had to be resized")
+            # Convert to PIL Image, resize, then convert back to NumPy array
+            img = np.moveaxis(clipped_orgfile, 0, -1)  # Rearrange (bands, x, y) to (x, y, bands)
+            img = Image.fromarray(img.astype('uint8'))  # Convert to unsigned 8-bit integer format
+            img_resized = img.resize((imsize, imsize), Image.LANCZOS)
+            clipped_orgfile = np.moveaxis(np.array(img_resized), -1, 0) 
+            # print(f"{building.osmid} had to be resized")
             
-
             output_path = Path(__file__).parent / "data" / "processed" 
             output_path.mkdir(parents=True, exist_ok=True)
             (output_path / "unlabelled").mkdir(exist_ok=True)
@@ -100,9 +99,7 @@ def cut_tif_into_building_photos(buildings, src, imsize: int):
             np.save(output_path / f'building_{building.osmid}.npy', clipped_orgfile)
 
             # to check the images:
-            img = np.moveaxis(clipped_orgfile, 0, -1)  # Rearrange (bands, x, y) to (x, y, bands)
-            img = Image.fromarray(img.astype('uint8'))
-            img.save(Path(__file__).parent / "data" / "processed" / "unlabelled" / f"building_{building.osmid}.png")
+            img_resized.save(Path(__file__).parent / "data" / "processed" / "unlabelled" / f"building_{building.osmid}.png")
         
     print(f"{len(out_of_bounds)} buildings were out of bounds")
 
