aea5be644a1bae657a3a6a2adc4aee137c1a5f9c
PhilippMaschi <phil.masch@hotmail.com>
2024-10-29 10:11:55 +0100

    uncomment

--- T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/solar-panel-classifier/run.py
+++ T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/solar-panel-classifier/run.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     
     # RunTask().make_masks()
     # RunTask().split_images()
-    # RunTask().train_classifier(retrain=True, warmup=2, max_epochs=100, patience=5)
+    RunTask().train_classifier(retrain=False, warmup=2, max_epochs=100, patience=5)
     # RunTask().train_both()
-    # RunTask().classify_new_data(retrained=True)
-    RunTask().segment_new_data()
+    RunTask().classify_new_data(retrained=False)
+    # RunTask().segment_new_data()
     # fire.Fire(RunTask)
